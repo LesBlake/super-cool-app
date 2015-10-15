4673a47a182d1be1d0696dafba1bc1c7882f1828
Les Blake <les.blake@gmail.com>
2015-10-14 21:50:53 -0600

    initial commit

--- class1/ViewController.swift
+++ class1/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var coolTitle: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var coolButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,10 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func makeCool(sender: AnyObject) {
+        coolTitle.hidden = false
+        coolBg.hidden = false
+        coolButton.hidden = true
+    }
 }
 
